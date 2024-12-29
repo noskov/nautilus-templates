dc1856fca7913a0af5b1d1c4edeca63c5fec5e16
Sergiy Noskov <noskov.org@gmail.com>
2024-12-29 02:20:13 +0100

    Update bash files

--- Templates/SH.sh
+++ Templates/SH.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script
 #
 # Description:
+#
 # Author: Sergiy Noskov <sergiy@noskov.org>
 # Version: 1.0.0
 # License: MIT License
-# Usage: sudo ./script
+#
+# Usage: sudo ./script [--help | -h]
+#
 # References:
 #   - https://google.github.io/styleguide/shellguide.html
 
@@ -23,10 +26,57 @@
 # Returns:
 #   Returned values other than the default exit status of the last command run.
 
-# Enable strict error handling: exit on error, unset variables, and pipeline failures.
+# Exit on error, undefined vars, and pipe failures
 set -euo pipefail
 
 
 
 
-exit 0
+
+# Function to display help
+show_help() {
+  cat << EOF
+
+Usage:
+
+Options:
+  --help, -h  Display this help message
+
+Description:
+
+Requirements:
+  - script
+
+Example usage after installation:
+  script
+
+EOF
+}
+
+# Function to handle script arguments
+handle_arguments() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      --help|-h)
+        show_help
+        exit 0
+        ;;
+      *)
+        echo "Invalid option: $1" >&2
+        show_help
+        exit 1
+        ;;
+    esac
+  done
+}
+
+# Main function to
+main() {
+  return 0
+}
+
+# Handle script arguments
+handle_arguments "$@"
+
+# Execute the main function
+main "$@"
